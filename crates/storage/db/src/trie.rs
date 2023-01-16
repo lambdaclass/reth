492ae1660cb6d8bcb9a3aa466eff6958eca13f18
lambdaclass-user <github@lambdaclass.com>
2023-01-16 16:37:48 -0300

    Change DBCodec for ReferenceNodeCodec

--- crates/storage/db/src/trie.rs
+++ crates/storage/db/src/trie.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 
 use hash256_std_hasher::Hash256StdHasher;
 use hash_db::{AsHashDB, Prefix};
+use reference_trie::ReferenceNodeCodec;
 use reth_primitives::{keccak256, H256};
 use trie_db::{HashDB, Hasher, NodeCodec, TrieDBMut, TrieLayout};
 
@@ -13,21 +14,20 @@ pub struct DBTrie<'this, DB: Database> {
     trie: TrieDBMut<'this, DBTrieLayout>,
 }
 
-impl<'this, DB: Database> DBTrie<'this, DB> {
-    fn get() {}
-}
+impl<'this, DB: Database> DBTrie<'this, DB> {}
 
 struct DBTrieLayout;
 
 impl TrieLayout for DBTrieLayout {
     const USE_EXTENSION: bool = true;
 
-    // TODO: modify this two?
+    // TODO: modify?
     const ALLOW_EMPTY: bool = false;
+    // I think non-inlined nodes aren't supported
     const MAX_INLINE_VALUE: Option<u32> = None;
 
     type Hash = KeccakHasher;
-    type Codec = DBCodec;
+    type Codec = ReferenceNodeCodec<Self::Hash>;
 }
 
 struct KeccakHasher;
@@ -41,66 +41,6 @@ impl Hasher for KeccakHasher {
 
     fn hash(x: &[u8]) -> Self::Out {
         keccak256(x)
-    }
-}
-
-struct DBCodec;
-
-impl NodeCodec for DBCodec {
-    type Error = crate::Error;
-
-    type HashOut = H256;
-
-    fn hashed_null_node() -> Self::HashOut {
-        todo!()
-    }
-
-    fn decode_plan(data: &[u8]) -> Result<trie_db::node::NodePlan, Self::Error> {
-        todo!()
-    }
-
-    fn is_empty_node(data: &[u8]) -> bool {
-        todo!()
-    }
-
-    fn empty_node() -> &'static [u8] {
-        todo!()
-    }
-
-    fn leaf_node(
-        partial: impl Iterator<Item = u8>,
-        number_nibble: usize,
-        value: trie_db::node::Value<'_>,
-    ) -> Vec<u8> {
-        todo!()
-    }
-
-    fn extension_node(
-        partial: impl Iterator<Item = u8>,
-        number_nibble: usize,
-        child_ref: trie_db::ChildReference<Self::HashOut>,
-    ) -> Vec<u8> {
-        todo!()
-    }
-
-    fn branch_node(
-        children: impl Iterator<
-            Item = impl std::borrow::Borrow<Option<trie_db::ChildReference<Self::HashOut>>>,
-        >,
-        value: Option<trie_db::node::Value<'_>>,
-    ) -> Vec<u8> {
-        todo!()
-    }
-
-    fn branch_node_nibbled(
-        partial: impl Iterator<Item = u8>,
-        number_nibble: usize,
-        children: impl Iterator<
-            Item = impl std::borrow::Borrow<Option<trie_db::ChildReference<Self::HashOut>>>,
-        >,
-        value: Option<trie_db::node::Value<'_>>,
-    ) -> Vec<u8> {
-        todo!()
     }
 }
 
